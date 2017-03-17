ee75217334dd4fc85e855e4efe45c6b032876585
IT83-2016 <IT83-2016@AR1-12.mshome.net>
2017-03-17 15:41:50 +0100

    dd

--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,5 +1,23 @@
 package drone;
 
-public class Drone {
+public  class Drone {
+   int x;
+   int y;
+   int z;
+   
+	@Override
+	public String toString() {
+		return "Drone [x=" + x + ", y=" + y + ", z=" + z + "]";
+	}
+
+	public Drone() {
+		super();
+	}
+	public abstract String moveUp()
+	{
+		
+	}
+
+	
 
 }
